fb1541f1bb3eee1d943ac7bb82b861d28e7a9835
dadihrannar1 <ddavid20@student.aau.dk>
2020-12-21 13:28:28 +0100

    rps progess

---
+++ src/rps.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <turtlesim>
+
+
+const int rock = 1;
+const int paper = 2;
+const int scissor = 3;
+
+int getTurtle1Choice(){
+
+       srand (time(NULL))
+        int turtle1Choice = rand() % 3 + 1;
+        return turtle1Choice =;
+
+   }
+
+int getTurtle2Choice(){
+
+       srand (time(NULL))
+        int turtle2Choice = rand() % 3 + 1;
+        return turtle2Choice =;
+
+   }
+
+void 
+
+int main(int argc, char **argv)
+{
+
+
+   std::cout <</*insert turtle name function*/ "and" /*insert turtlename function2*/ "have comenced battle">>;
+
+
+}
