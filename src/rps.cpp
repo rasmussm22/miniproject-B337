c6aba923501e65aac71ae4070d09fa4991dac8f9
dadihrannar1 <ddavid20@student.aau.dk>
2020-12-22 15:45:12 +0100

    here is the thing daniel

--- src/rps.cpp
+++ src/rps.cpp
@@ -7,6 +7,28 @@
 #include <unistd.h> ///For the sleep//
 #include <std_msgs/Int32.h>
 
+/*
+void t1cb(const std_msgs::Int32::ConstPtr& msg)
+{
+std::cout << "Content: " << msg->data << std::endl;
+}
+
+void t2cb(const std_msgs::Int32::ConstPtr& msg)
+{
+std::cout << "Content: " << msg->data << std::endl;
+}
+*/
+//testing
+int rock = 1;
+int paper = 2;
+int scissor = 3;
+
+int getTurtle_choice()
+{
+    int turtle_choice = rand() % 3 + 1;
+    return turtle_choice;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rps");
@@ -15,12 +37,15 @@ int main(int argc, char **argv)
     ros::Publisher t1_pub = n.advertise<std_msgs::Int32>("t1", 1);
     ros::Publisher t2_pub = n.advertise<std_msgs::Int32>("t2", 1);
 
+    //ros::Subscriber t1_sub = n.subscribe("t1", 1, t1cb);
+    //ros::Subscriber t2_sub = n.subscribe("t2", 1, t2cb);  //testing
+
     std::srand(time(NULL));
 
     int Turtle1_NumberOfWins = 0;
     int Turtle2_NumberOfWins = 0;
-    std::string turtle1;
-    std::string turtle2;
+    int turtle1_choice;
+    int turtle2_choice;
     int t1 = 0;
     int t2 = 0;
     bool determine_Looser;
@@ -28,71 +53,42 @@ int main(int argc, char **argv)
     while (Turtle1_NumberOfWins < 3 && Turtle2_NumberOfWins < 3)
     {
 
-        int turtle1_control = rand() % 3 + 1;
-        int turtle2_control = rand() % 3 + 1;
-
-        if (turtle1_control == 1)
-        {
-            turtle1 = "rock";
-        }
-        else if (turtle1_control == 2)
-        {
-            turtle1 = "paper";
-        }
-        else if (turtle1_control == 3)
-        {
-            turtle1 = "scissor";
-        }
-
-        if (turtle2_control == 1)
-        {
-            turtle2 = "rock";
-        }
-        else if (turtle2_control == 2)
-        {
-            turtle2 = "paper";
-        }
-        else if (turtle2_control == 3)
-        {
-            turtle2 = "scissor";
-        }
-
-        std::cout << "Turtle1 chooses: " << turtle1 << std::endl;
-        std::cout << "Turtle2 chooses: " << turtle2 << std::endl;
+        turtle1_choice = getTurtle_choice();
+        turtle2_choice = getTurtle_choice();
 
         ///////Outputting outcome of battle, and counting score///////////
 
-        if (turtle1 == "rock" && turtle2 == "scissor")
+        if (turtle1_choice == rock && turtle2_choice == scissor)
         {
             Turtle1_NumberOfWins++;
             std::cout << "Turtle 1 won! Turtle1 chose rock while turtle2 chose scissor" << std::endl;
             std::cout << "Turtle1 gets one point" << std::endl;
         }
-        else if (turtle1 == "scissor" && turtle2 == "paper")
+        else if (turtle1_choice == scissor && turtle2_choice == paper)
         {
             Turtle1_NumberOfWins++;
             std::cout << "Turtle 1 won! Turtle1 chose scissor while turtle2 chose paper" << std::endl;
             std::cout << "Turtle1 gets one point" << std::endl;
         }
-        else if (turtle1 == "paper" && turtle2 == "rock")
+        else if (turtle1_choice == paper && turtle2_choice == rock)
         {
             Turtle1_NumberOfWins++;
             std::cout << "Turtle 1 won! Turtle1 chose paper while turtle2 chose rock" << std::endl;
             std::cout << "Turtle1 gets one point" << std::endl;
         }
-        else if (turtle1 == "rock" && turtle2 == "paper")
+        else if (turtle1_choice == rock && turtle2_choice == paper)
         {
             Turtle2_NumberOfWins++;
             std::cout << "Turtle 2 won! Turtle1 chose rock while turtle2 chose paper" << std::endl;
             std::cout << "Turtle2 gets one point" << std::endl;
         }
-        else if (turtle1 == "scissor" && turtle2 == "rock")
+        else if (turtle1_choice == scissor && turtle2_choice == rock)
         {
             Turtle2_NumberOfWins++;
             std::cout << "Turtle 2 won! Turtle1 chose scissor while turtle2 chose rock" << std::endl;
             std::cout << "Turtle2 gets one point" << std::endl;
         }
-        else if (turtle1 == "paper" && turtle2 == "scissor")
+        else if (turtle1_choice == paper && turtle2_choice == scissor)
         {
             Turtle2_NumberOfWins++;
             std::cout << "Turtle 2 won! Turtle1 chose paper while turtle2 chose scissor" << std::endl;
@@ -106,25 +102,25 @@ int main(int argc, char **argv)
 
         std::cout << "Score: Turtle1 = " << Turtle1_NumberOfWins << ", Turtle2 = " << Turtle2_NumberOfWins << std::endl;
 
-        sleep(2);
+        sleep(1);
     }
 
-    if (Turtle1_NumberOfWins = 3)
+    if (Turtle1_NumberOfWins == 3)
     {
         std::cout << "Turtle1 is the champion!" << std::endl;
         determine_Looser = false;
     }
-    else if (Turtle2_NumberOfWins = 3)
+    else if (Turtle2_NumberOfWins == 3)
     {
         std::cout << "Turtle2 is the champion!" << std::endl;
         determine_Looser = true;
     }
 
-    if (determine_Looser = true)
+    if (determine_Looser == true)
     {
         t1--;
     }
-    else if (determine_Looser = false)
+    else if (determine_Looser == false)
     {
         t2--;
     }
