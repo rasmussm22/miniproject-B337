b562a26781e5dd015b211c8e8594dc6c3cea4dd7
dadihrannar1 <ddavid20@student.aau.dk>
2020-12-22 11:05:39 +0100

    big changes

--- src/rps.cpp
+++ src/rps.cpp
@@ -1,77 +1,117 @@
 #include <iostream>
 #include <ros/ros.h>
-#include <turtlesim/Pose.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>  //random generator i think?//
+#include <time.h>   //For the random generator//
+#include <unistd.h> ///For the sleep//
 
-std::string   t1 = "turtle1 ";
-std::string   t2 = "turtle2 ";
-
-int getTurtle1Choice()
+int main(int argc, const char *argv[])
 {
+    std::srand(time(NULL));
 
-    int turtle1Choice = rand() % 3 + 1;
-    return turtle1Choice;
-}
+    int Turtle1_NumberOfWins = 0;
+    int Turtle2_NumberOfWins = 0;
+    std::string turtle1;
+    std::string turtle2;
 
-int getTurtle2Choice()
-{
 
-    int turtle2Choice = rand() % 3 + 1;
-    return turtle2Choice;
-}
-
-bool determineWinner(int getTurtle1Choice, int getTurtle2Choice)
-{
-    bool tie = false;
-
-    const int rock = 1;
-    const int paper = 2;
-    const int scissor = 3;
-
-    if (getTurtle1Choice == rock && getTurtle2Choice == paper)
+    while (Turtle1_NumberOfWins < 3 && Turtle2_NumberOfWins < 3)
     {
-        std::cout <<  t2 << "Wins! Paper sufficates rock!" << std::endl;
+
+        int turtle1_control = rand() % 3 + 1;
+        int turtle2_control = rand() % 3 + 1;
+
+        if (turtle1_control == 1)
+        {
+            turtle1 = "rock";
+        }
+        else if (turtle1_control == 2)
+        {
+            turtle1 = "paper";
+        }
+        else if (turtle1_control == 3)
+        {
+            turtle1 = "scissor";
+        }
+
+        if (turtle2_control == 1)
+        {
+            turtle2 = "rock";
+        }
+        else if (turtle2_control == 2)
+        {
+            turtle2 = "paper";
+        }
+        else if (turtle2_control == 3)
+        {
+            turtle2 = "scissor";
+        }
+
+        std::cout << "Turtle1 chooses: " << turtle1 << std::endl;
+        std::cout << "Turtle2 chooses: " << turtle2 << std::endl;
+
+        ///////Outputting outcome of battle, and counting score///////////
+
+       
+        if (turtle1 == "rock" && turtle2 == "scissor")
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose rock while turtle2 chose scissor" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1 == "scissor" && turtle2 == "paper")
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose scissor while turtle2 chose paper" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1 == "paper" && turtle2 == "rock")
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose paper while turtle2 chose rock" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1 == "rock" && turtle2 == "paper")
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose rock while turtle2 chose paper" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        }
+        else if (turtle1 == "scissor" && turtle2 == "rock")
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose scissor while turtle2 chose rock" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        }
+        else if (turtle1 == "paper" && turtle2 == "scissor")
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose paper while turtle2 chose scissor" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        } 
+        else
+        {
+            std::cout << "Its a tie! The turtles picked the same thing " << std::endl;
+            std::cout << "No points are given" << std::endl;
+        }
+
+        std::cout <<"Score: Turtle1 = " << Turtle1_NumberOfWins << ", Turtle2 = " << Turtle2_NumberOfWins << std::endl;
+
+
+        sleep(2);
     }
-    else if (getTurtle2Choice == rock && getTurtle1Choice == paper)
-    {
-        std::cout << t1 << "Wins! Paper sufficates rock!" << std::endl;
-    }
-    else if (getTurtle1Choice == scissor && getTurtle2Choice == paper)
-    {
-        std::cout << t1 << "Wins! Scissor sepirates paper leaving it in 2!" << std::endl;
-    }
-    else if (getTurtle2Choice == scissor && getTurtle1Choice == paper)
-    {
-        std::cout << t2 << "Wins! Scissor sepirates paper leaving it in 2!" << std::endl;
-    }
-    else if (getTurtle1Choice == rock && getTurtle2Choice == scissor)
-    {
-        std::cout << t1 << "Wins! Rock crushes the scissors into a pulp!" << std::endl;
-    }
-    else if (getTurtle2Choice == rock && getTurtle1Choice == scissor)
-    {
-        std::cout << t2 << "Wins! Rock crushes the scissors into a pulp!" << std::endl;
-    }
-    else
-    {
-        std::cout << "Tie" << std::endl;
-        tie = true;
-    }
 
-    return tie;
-}
-
-int main(int argc, char **argv)
-{
-    srand(time(NULL));
-
-    std::cout << t1 << "and " << t2 << "have comenced battle " << std::endl;
-
-    bool tie;
-
-    do
+    if (Turtle1_NumberOfWins = 3)
     {
-        tie = determineWinner(getTurtle1Choice(), getTurtle2Choice());
-    } while (tie == true);
+        std::cout << "Turtle1 is the champion!" << std::endl;
+    }
+    else if (Turtle2_NumberOfWins = 3)
+    {
+        std::cout << "Turtle2 is the champion!" << std::endl;
+    }
 
     return 0;
 }
