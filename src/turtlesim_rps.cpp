929bb8d1ab899d5233ef259457a092065c612be6
Stefan Vejrum <svejru20@student.aau.dk>
2020-12-22 13:01:59 +0100

    Simplest user input ever

--- src/turtlesim_rps.cpp
+++ src/turtlesim_rps.cpp
@@ -1,10 +1,15 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
+#include <iostream>
 
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+int t;
+cout << "How many turtles are facing off: "; 
+cin >> t; // Insert amount of turtles
+cout << t << " turtles are facing off";
     return 0;
 }
