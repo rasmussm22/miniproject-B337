6f47fa0122b84d841a41da10e5167edceeb6b769
Stefan Vejrum <svejru20@student.aau.dk>
2020-12-22 14:29:04 +0100

    14:28 - 22-12-2020

--- src/turtlesim_rps.cpp
+++ src/turtlesim_rps.cpp
@@ -1,9 +1,26 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
+#include <turtlesim/Spawn.h>
 #include <iostream>
+#include <cstdlib>
+
 
 using namespace std;
+ros::NodeHandle * p_node_handle;
+
+// Spawn_turtle snippet from kill turtle start
+void spawn_turtle(int n)
+{
+    ros::ServiceClient spawn_client = 
+    p_node_handle->serviceClient<turtlesim::Spawn>("/spawn");
+    turtlesim::Spawn spawn_message;
+    spawn_message.request.name = "turtle2";
+    spawn_message.request.x = rand() % 11;
+    spawn_message.request.y = rand() % 11;
+    spawn_client.call(spawn_message);
+}
+// Snippet end
 
 int main(int argc, char const *argv[])
 {
