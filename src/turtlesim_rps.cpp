ec25acd5d78a2afaafe428db04a7146f29362317
dadihrannar1 <ddavid20@student.aau.dk>
2020-12-27 10:28:26 +0100

    main node changes

--- src/turtlesim_rps.cpp
+++ src/turtlesim_rps.cpp
@@ -1,48 +1,177 @@
+#include <iostream>
 #include <ros/ros.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>  //random generator i think?//
+#include <time.h>   //For the random generator//
+#include <unistd.h> ///For the sleep//
+#include <std_msgs/Int32.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
 #include <turtlesim/Spawn.h>
-#include <iostream>
 #include <cstdlib>
-
-
-using namespace std;
-ros::NodeHandle * p_node_handle;
-
-// Spawn_turtle snippet from kill turtle start
-void spawn_turtle(char n)
+#include <turtlesim/Kill.h>
+/*
+void t1cb(const std_msgs::Int32::ConstPtr& msg)
 {
-    ros::ServiceClient spawn_client = 
-    p_node_handle->serviceClient<turtlesim::Spawn>("/spawn");
-    turtlesim::Spawn spawn_message;
-    spawn_message.request.name = "";
-    spawn_message.request.x = rand() % 11;
-    spawn_message.request.y = rand() % 11;
-    spawn_client.call(spawn_message);
+std::cout << "Content: " << msg->data << std::endl;
 }
-// Snippet end
-// char t;
-int yeet=0;
-int main(int argc, char *argv[]){
 
-int t;
-//char yeet =0;
+void t2cb(const std_msgs::Int32::ConstPtr& msg)
+{
+std::cout << "Content: " << msg->data << std::endl;
+}
+*/
+//testing
 
-cout << "How many turtles are facing off: "; 
-cin >> t; // Insert amount of turtles
-cout << t << " turtles are facing off";
+turtlesim::Pose turtle1_Pose;
+turtlesim::Pose turtle2_Pose;
+int rock = 1;
+int paper = 2;
+int scissor = 3;
 
-// don't know don't care 
-// without them it says segmentation fault
-ros::init( argc, argv, "turtle_spawn" );
-ros::NodeHandle node_handle;
-p_node_handle =&node_handle;
+void turtle1Callback(const turtlesim::Pose::ConstPtr &msg)
+{
+    turtle1_Pose = *msg;
+}
 
-//Spend four hours to get this 3 line code T_T
-for(yeet=0 ; yeet<t; yeet=yeet+1 )
+void turtle2Callback(const turtlesim::Pose::ConstPtr &msg)
+{
+    turtle2_Pose = *msg;
+}
+
+
+int getTurtle_choice()
+{
+    int turtle_choice = rand() % 3 + 1;
+    return turtle_choice;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "rps");
+    ros::NodeHandle n;
+
+    ros::Rate test(1);
+
+    ros::Subscriber pose1_subscriber = n.subscribe("/turtle1/pose", 100, turtle1Callback);
+    ros::Subscriber pose2_subscriber = n.subscribe("/turtle2/pose", 100, turtle2Callback);
+
+    ros::Publisher t1_pub = n.advertise<std_msgs::Int32>("t1", 1);
+    ros::Publisher t2_pub = n.advertise<std_msgs::Int32>("t2", 1);
+
+    //ros::Subscriber t1_sub = n.subscribe("t1", 1, t1cb);
+    //ros::Subscriber t2_sub = n.subscribe("t2", 1, t2cb);  //testing
+
+    double distance = 0;
+    
+    test.sleep();
+
+    do
     {
-spawn_turtle(0);
-}
+    double x_distance = turtle1_Pose.x - turtle2_Pose.x;
+    double y_distance = turtle1_Pose.y - turtle2_Pose.y;
+    distance = hypot(x_distance, y_distance);
+
+    std::cout << distance << std::endl;
+    std::cout << turtle1_Pose.x << std::endl;
+    std::cout << turtle1_Pose.y << std::endl;
+
+    std::cout << turtle2_Pose.x << std::endl;
+    std::cout << turtle2_Pose.y << std::endl;
+
+
+    } while (distance > 1);
+    
+    
+
+
+    std::srand(time(NULL));
+
+    int Turtle1_NumberOfWins = 0;
+    int Turtle2_NumberOfWins = 0;
+    int turtle1_choice;
+    int turtle2_choice;
+    int t1 = 0;
+    int t2 = 0;
+
+    while (Turtle1_NumberOfWins < 3 && Turtle2_NumberOfWins < 3)
+    {
+
+        turtle1_choice = getTurtle_choice();
+        turtle2_choice = getTurtle_choice();
+
+        ///////Outputting outcome of battle, and counting score///////////
+
+        if (turtle1_choice == rock && turtle2_choice == scissor)
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose rock while turtle2 chose scissor" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1_choice == scissor && turtle2_choice == paper)
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose scissor while turtle2 chose paper" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1_choice == paper && turtle2_choice == rock)
+        {
+            Turtle1_NumberOfWins++;
+            std::cout << "Turtle 1 won! Turtle1 chose paper while turtle2 chose rock" << std::endl;
+            std::cout << "Turtle1 gets one point" << std::endl;
+        }
+        else if (turtle1_choice == rock && turtle2_choice == paper)
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose rock while turtle2 chose paper" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        }
+        else if (turtle1_choice == scissor && turtle2_choice == rock)
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose scissor while turtle2 chose rock" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        }
+        else if (turtle1_choice == paper && turtle2_choice == scissor)
+        {
+            Turtle2_NumberOfWins++;
+            std::cout << "Turtle 2 won! Turtle1 chose paper while turtle2 chose scissor" << std::endl;
+            std::cout << "Turtle2 gets one point" << std::endl;
+        }
+        else
+        {
+            std::cout << "Its a tie! The turtles picked the same thing " << std::endl;
+            std::cout << "No points are given" << std::endl;
+        }
+
+        std::cout << "Score: Turtle1 = " << Turtle1_NumberOfWins << ", Turtle2 = " << Turtle2_NumberOfWins << std::endl;
+
+        sleep(1.5);
+    }
+
+    if (Turtle1_NumberOfWins == 3)
+    {
+        std::cout << "Turtle1 is the champion!" << std::endl;
+        t1--;
+    }
+    else if (Turtle2_NumberOfWins == 3)
+    {
+        std::cout << "Turtle2 is the champion!" << std::endl;
+        t2--;
+    }
+
+    std_msgs::Int32 msg1;
+    std_msgs::Int32 msg2;
+
+    msg1.data = t1;
+    msg2.data = t2;
+
+    t1_pub.publish(msg1);
+    t2_pub.publish(msg2);
+
+
+
+
 
     return 0;
-}
