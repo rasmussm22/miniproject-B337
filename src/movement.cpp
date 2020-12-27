3d7baa290aa77282591de25b0196fef8dafac497
dadihrannar1 <ddavid20@student.aau.dk>
2020-12-27 14:29:24 +0100

    comments and such

--- src/movement.cpp
+++ src/movement.cpp
@@ -8,44 +8,43 @@
 #include <std_msgs/Int32.h>
 #include <sstream>
 #include <string>
+#include <turtlesim/TeleportAbsolute.h>
 #include <turtlesim/SetPen.h>
 
 geometry_msgs::Twist cmd_vel_message1;
 geometry_msgs::Twist cmd_vel_message2;
 float velocity_gain = 0.1;
 float angular_gain = 0.7;
+//velocity for the turtles
 float battle_distance = 1;
+//how far the turtles are from each other when they commence the battle
 turtlesim::Pose turtle1_Pose;
 turtlesim::Pose turtle2_Pose;
+//position of the turtles
 ros::NodeHandle *p_node_handle;
+//pointer node handle, so it can be used outside of the main int
 int t1 = 1;
 int t2 = 1;
+//starting lifes for the turtles
 
 void kill_turtle(std::string n)
 {
-    //std::stringstream ss;
-    //ss << "turtle" << n;
     turtlesim::Kill kill_message;
     kill_message.request.name = n;
     ros::ServiceClient kill_client = p_node_handle->serviceClient<turtlesim::Kill>("/kill");
     kill_client.call(kill_message);
 }
+//kill command for the turtles
 
 void t1Callback(const std_msgs::Int32::ConstPtr &msg)
 {
     t1 = msg->data;
-
-    std::cout << "I heard from t1Callback " << msg->data << std::endl;
-
+    //assigning the data to the t1 and t2 
 }
-
 
 void t2Callback(const std_msgs::Int32::ConstPtr &msg)
 {
     t2 = msg->data;
-
-    std::cout << "I heard from t2Callback " << msg->data << std::endl;
-
 }
 
 void turtle1Callback(const turtlesim::Pose::ConstPtr &msg)
@@ -114,14 +113,16 @@ void turtle2Callback(const turtlesim::Pose::ConstPtr &msg)
         cmd_vel_message2.angular.z = angular_gain * angular_difference;
     }
 }
+//movement and also knowing when it is battle range
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "get_close");
+    //intilating ros
 
     ros::NodeHandle n;
     p_node_handle = &n;
-    
+
     srand(time(NULL));
 
     ros::ServiceClient spawn_client = n.serviceClient<turtlesim::Spawn>("/spawn");
@@ -130,26 +131,33 @@ int main(int argc, char *argv[])
     spawn_message.request.x = rand() % 11;
     spawn_message.request.y = rand() % 11;
     spawn_client.call(spawn_message);
+    //spawnign the second turtle
 
     ros::ServiceClient pen_client = n.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
     ros::ServiceClient pen_client2 = n.serviceClient<turtlesim::SetPen>("/turtle2/set_pen");
-    
 
-     turtlesim::SetPen pen_srv;
-     pen_srv.request.off = true;
-     pen_client.call(pen_srv);
-     pen_client2.call(pen_srv);
+    turtlesim::SetPen pen_srv;
+    pen_srv.request.off = true;
+    pen_client.call(pen_srv);
+    pen_client2.call(pen_srv);
+    //erasing the pen line 
 
-
-    
+    ros::ServiceClient teleport_client = n.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
+    turtlesim::TeleportAbsolute srv;
+    srv.request.x = rand() % 11;
+    srv.request.y = rand() % 11;
+    teleport_client.call(srv);
     ros::Publisher cmd_vel_pub1 = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
     ros::Publisher cmd_vel_pub2 = n.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 1);
+    //teleporting the first turtle to a random place so both turtles are in a random place
 
     ros::Subscriber pose1_subscriber = n.subscribe("/turtle1/pose", 100, &turtle1Callback);
     ros::Subscriber pose2_subscriber = n.subscribe("/turtle2/pose", 100, &turtle2Callback);
+    //finding out the position of the turtles
 
     ros::Subscriber t1_subscriber = n.subscribe("t1", 1, &t1Callback);
     ros::Subscriber t2_subscriber = n.subscribe("t2", 1, &t2Callback);
+    //subscribing to the lives of the turtles
 
     ros::Rate loop_rate(10);
     while (ros::ok())
@@ -160,15 +168,15 @@ int main(int argc, char *argv[])
 
         if (t1 == 0)
         {
-        kill_turtle("turtle1");
-        t1=1;
+            kill_turtle("turtle1");
+            t1 = 1;
         }
         else if (t2 == 0)
         {
-        kill_turtle("turtle2");
-        t2=1;
+            kill_turtle("turtle2");
+            t2 = 1;
         }
-
+        //killing the turtle that looses than making the life 1 again so the terminal doesn't get filled with messages
         ros::spinOnce();
     }
 
--- src/rps.cpp
+++ src/rps.cpp
@@ -2,33 +2,23 @@
 #include <ros/ros.h>
 #include <string>
 #include <stdlib.h>
-#include <stdio.h>  //random generator i think?//
-#include <time.h>   //For the random generator//
-#include <unistd.h> ///For the sleep//
+#include <stdio.h>  
+#include <time.h>   
+#include <unistd.h> 
 #include <std_msgs/Int32.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
 #include <turtlesim/Spawn.h>
 #include <cstdlib>
 #include <turtlesim/Kill.h>
-/*
-void t1cb(const std_msgs::Int32::ConstPtr& msg)
-{
-std::cout << "Content: " << msg->data << std::endl;
-}
 
-void t2cb(const std_msgs::Int32::ConstPtr& msg)
-{
-std::cout << "Content: " << msg->data << std::endl;
-}
-*/
-//testing
 
 turtlesim::Pose turtle1_Pose;
 turtlesim::Pose turtle2_Pose;
 int rock = 1;
 int paper = 2;
 int scissor = 3;
+//giving the rock, paper, scissors a diffirent number
 
 void turtle1Callback(const turtlesim::Pose::ConstPtr &msg)
 {
@@ -46,26 +36,29 @@ int getTurtle_choice()
     int turtle_choice = rand() % 3 + 1;
     return turtle_choice;
 }
+//making the turtles able to pick rock, paper or scissors randomly
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rps");
     ros::NodeHandle n;
 
-    ros::Rate test(1);
+    ros::Rate delay(1);
 
     ros::Subscriber pose1_subscriber = n.subscribe("/turtle1/pose", 100, &turtle1Callback);
     ros::Subscriber pose2_subscriber = n.subscribe("/turtle2/pose", 100, &turtle2Callback);
+    //subscribing to the postition of the turtles to see when to start the game
+
 
     ros::Publisher t1_pub = n.advertise<std_msgs::Int32>("t1", 1);
     ros::Publisher t2_pub = n.advertise<std_msgs::Int32>("t2", 1);
-
-    //ros::Subscriber t1_sub = n.subscribe("t1", 1, t1cb);
-    //ros::Subscriber t2_sub = n.subscribe("t2", 1, t2cb);  //testing
+    //subscribing to the lifes of the turtles
 
     double distance = 0;
     
-    test.sleep();
+    delay.sleep();
+    //delaying the game so that when the game dosent start when turtle1 teleports in the beginning
+
 
     do
     {
@@ -76,19 +69,14 @@ int main(int argc, char **argv)
     double x_distance = turtle1_Pose.x - turtle2_Pose.x;
     double y_distance = turtle1_Pose.y - turtle2_Pose.y;
     distance = hypot(x_distance, y_distance);
-    /*
-    std::cout << distance << std::endl;
-    std::cout << turtle1_Pose.x << std::endl;
-    std::cout << turtle1_Pose.y << std::endl;
+    //calculating the distance between the two turtles
 
-    std::cout << turtle2_Pose.x << std::endl;
-    std::cout << turtle2_Pose.y << std::endl;
-    */
 
     } while (distance > 1);
     
 
     std::srand(time(NULL));
+    //a statement that makes it so that every choice that the turtle makes is "random"
 
     int Turtle1_NumberOfWins = 0;
     int Turtle2_NumberOfWins = 0;
@@ -149,7 +137,8 @@ int main(int argc, char **argv)
 
         std::cout << "Score: Turtle1 = " << Turtle1_NumberOfWins << ", Turtle2 = " << Turtle2_NumberOfWins << std::endl;
 
-        sleep(1.5);
+        sleep(2.5);
+        //the time between rounds (secs)
     }
 
     if (Turtle1_NumberOfWins == 3)
@@ -162,6 +151,8 @@ int main(int argc, char **argv)
         std::cout << "Turtle2 is the champion!" << std::endl;
         t1--;
     }
+    //finding out who is gets 3 wins first then it gives the looser -1 making its lives 0 and killing it
+
 
     std_msgs::Int32 msg1;
     std_msgs::Int32 msg2;
@@ -171,6 +162,7 @@ int main(int argc, char **argv)
 
     t1_pub.publish(msg1);
     t2_pub.publish(msg2);
+    //publishing the lives of the turtles
 
     ros::spinOnce();
     
